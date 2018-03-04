ca14d4ccd44c7b6d6afa0c099d6d30fbc8d4f19d
jiangying <13814959143@sina.cn>
2018-03-04 23:12:36 +0800

    change jvmtest to HeapOomMock add PermGenOomMock StackErrorMock StringOomMock(三个版本永久代的差异)

--- JVM/src/com/my/jvm/HeapOomMock.java
+++ JVM/src/com/my/jvm/HeapOomMock.java
@@ -2,10 +2,13 @@ package com.my.jvm;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import com.my.size.SizeOfObject;
-
-public class Jvmtest {
+/**
+ * @run parameters
+ * -javaagent:d:\workspace\java\knowledge\JVM\SizeOfObject.jar -Xms10M -Xmx20M -XX:MetaspaceSize=8M -XX:MaxMetaspaceSize=16M -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Xloggc:gc.log
+ * @author Administrator
+ *
+ */
+public class HeapOomMock {
 	/**Bit（比特，一个二进制位，通信常用的单位 0,1）
 
 		Byte（字节，由八个二进制位组成，是计算机中表示存储空间的最基本容量单位） K（千，存储空间的容量单位, kilobyte，1K=1024字节）
@@ -18,16 +21,16 @@ public class Jvmtest {
 	**/
 	
 	//int 32bit=4byte 
-	private static int[] intArr = new int[1024*1024/4];
 	
 	public static void main(String[] args) throws InterruptedException {
 		List<int[]> list = new ArrayList<int[]>();
-		Jvmtest jt = new Jvmtest();
 		Thread.sleep(5*1000);
 		try {
 			while(true) {
 				Thread.sleep(1000);
+				//剩余内存
 				long initm=Runtime.getRuntime().freeMemory()/1024/1024;
+				//总内存
 				long totalm = Runtime.getRuntime().totalMemory()/1024/1024;
 				System.out.println("free :" + initm + "M" + "---total : " + totalm + "M");//sysout，然后ALT+/ 
 				list.add(new int[1024*1024/4]);
